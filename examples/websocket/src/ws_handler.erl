a94722a060a4ce09ff6064cbe3a59961a35760df
firsou <guschefi@student.gu.se>
2017-11-22 22:00:45 +0100

    Fixed bug

--- examples/websocket/src/ws_handler.erl
+++ examples/websocket/src/ws_handler.erl
@@ -12,16 +12,17 @@ websocket_init(State) ->
 	erlang:start_timer(1000, self(), <<"Hello!">>),
 	{ok, State}.
 
-websocket_handle({text, Msg}, State) ->
+websocket_handle({text, "cookie"}, State) ->
 	NewValue = integer_to_list(rand:uniform(1000000)),
 	Req1 = cowboy_req:set_resp_cookie(<<"server">>, NewValue,
+	Req, #{path => <<"/">>}),
 	#{client := ClientCookie, server := ServerCookie}
 		= cowboy_req:match_cookies([{client, [], <<>>}, {server, [], <<>>}], Req1),
-	Req = cowboy_req:reply(200, #{
+	Req0 = cowboy_req:reply(200, #{
 		<<"content-type">> => <<"text/html">>
 	}),
-	{AllCookies, Req1} = cowboy_req:cookies(Req),
-	{reply, {text, << {AllCookies, ClientCookie, ServerCookie}, Msg/binary >>}, State};
+	{AllCookies, Req2} = cowboy_req:cookies(Req0),
+	{reply, {text, << {AllCookies, ClientCookie, ServerCookie} Msg/binary >>}, State};
 websocket_handle({text, Msg}, State) ->
 	{reply, {text, << "That's what she said! ", Msg/binary >>}, State};
 websocket_handle(_Data, State) ->
