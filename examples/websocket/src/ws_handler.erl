682e40b22ad254abce8f19a775a48b498a679098
firsou <guschefi@student.gu.se>
2017-11-22 22:05:08 +0100

    Changed function params in ws_handler

--- examples/websocket/src/ws_handler.erl
+++ examples/websocket/src/ws_handler.erl
@@ -12,7 +12,7 @@ websocket_init(State) ->
 	erlang:start_timer(1000, self(), <<"Hello!">>),
 	{ok, State}.
 
-websocket_handle({text, "cookie"}, State) ->
+websocket_handle({text, Msg}, State) ->
 	NewValue = integer_to_list(rand:uniform(1000000)),
 	Req1 = cowboy_req:set_resp_cookie(<<"server">>, NewValue),
 	#{client := ClientCookie, server := ServerCookie}
@@ -20,7 +20,7 @@ websocket_handle({text, "cookie"}, State) ->
 	Req0 = cowboy_req:reply(200, #{
 		<<"content-type">> => <<"text/html">>
 	}),
-	{AllCookies, Req2} = cowboy_req:cookies(Req0),
+	{AllCookies, Req1} = cowboy_req:cookies(Req0),
 	{reply, {text, << {AllCookies, ClientCookie, ServerCookie}, Msg/binary >>}, State};
 websocket_handle({text, Msg}, State) ->
 	{reply, {text, << "That's what she said! ", Msg/binary >>}, State};
