d1adaef0babdf6252928744987140b4db25d8590
firsou <guschefi@student.gu.se>
2017-11-22 23:01:15 +0100

    Back to 0

--- examples/websocket/src/ws_handler.erl
+++ examples/websocket/src/ws_handler.erl
@@ -9,20 +9,13 @@ init(Req, Opts) ->
 	{cowboy_websocket, Req, Opts}.
 
 websocket_init(State) ->
-	erlang:start_timer(1000, self(), <<"Hello!">>),
+	SessionID = generate_session_id(),
+	Req = cowboy_req:set_resp_cookie(<<"sessionid">>, SessionID, Req0,
+    #{max_age => 3600}),
 	{ok, State}.
 
 websocket_handle({text, Msg}, State) ->
-  NewValue = integer_to_list(rand:uniform(1000000)),
-  Req1 = cowboy_req:set_resp_cookie(<<"server">>, NewValue),
-  #{client := ClientCookie, server := ServerCookie}
-    = cowboy_req:match_cookies([{client, [], <<>>}, {server, [], <<>>}], Req1),
-  Req0 = cowboy_req:reply(200, #{
-    <<"content-type">> => <<"text/html">>
-  }),
-  {AllCookies, Req1} = cowboy_req:cookies(Req0),
-  CoolString = lists:flatten(io_lib:format("~p", [{AllCookies, ClientCookie, ServerCookie}])),
-  {reply, {text, << CoolString, Msg/binary >>}, State};
+	{reply, {text, << "That's what she said! ", Msg/binary >>}, State};
 websocket_handle(_Data, State) ->
 	{ok, State}.
 
