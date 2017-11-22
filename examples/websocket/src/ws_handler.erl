6c0dd6914f2b246be62dd797ef7d3bc0427430f6
firsou <guschefi@student.gu.se>
2017-11-22 21:00:06 +0100

    Added cookie support in websocket example

--- examples/websocket/src/ws_handler.erl
+++ examples/websocket/src/ws_handler.erl
@@ -12,13 +12,13 @@ websocket_init(State) ->
 	erlang:start_timer(1000, self(), <<"Hello!">>),
 	{ok, State}.
 
+websocket_handle({text, "cookie"}, State) ->
+	{AllCookies, Req2} = cowboy_req:cookies(Req),
+	{reply, {text, << AllCookies, Msg/binary >>}, State};
 websocket_handle({text, Msg}, State) ->
 	{reply, {text, << "That's what she said! ", Msg/binary >>}, State};
 websocket_handle(_Data, State) ->
 	{ok, State}.
 
-websocket_info({timeout, _Ref, Msg}, State) ->
-	erlang:start_timer(1000, self(), <<"How' you doin'?">>),
-	{reply, {text, Msg}, State};
 websocket_info(_Info, State) ->
 	{ok, State}.
